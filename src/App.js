1319f7c88c6a3a3e18b4ee3cb67b706e9a03d3ea
lehuynhtuan <meowmeowlht@gmail.com>
2022-09-22 23:11:45 +0700

    layout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BaiTapLayOut from './components/BaiTapLayOut';
+
+function App() {
+  return (
+    <div>
+      <BaiTapLayOut/>
+    </div>
+  );
+}
+
+export default App;
